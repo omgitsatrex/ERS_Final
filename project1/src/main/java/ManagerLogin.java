d3c76b14b88324db0e274a1b877a287bbc976ac5
William Fuentes <fuenteswilliam0326@gmail.com>
2022-03-05 12:23:24 -0800

    Making servlets in for manager login

--- project1/src/main/java/ManagerLogin.java
+++ project1/src/main/java/ManagerLogin.java
@@ -7,40 +7,35 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
-public class LoginEmployeeServlet extends HttpServlet {
-
+public class ManagerLogin extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
-        PrintWriter out=response.getWriter();
+        PrintWriter out = response.getWriter();
 
-        String username=request.getParameter("username");
-        String password=request.getParameter("password");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
 
 //use with  HttpSession
 
-        request.getRequestDispatcher("navbar.html").include(request,response);
+        request.getRequestDispatcher("navbar.html").include(request, response);
         //when user enters correct details
-        if(username.equals("username") && password.equals("password")) {
+        if (username.equals("username") && password.equals("password")) {
             out.print("<br>You are successfully logged in");
             out.println("<br>Welcome " + username);
 
 
-            HttpSession session= request.getSession();
-            session.setAttribute("uname",username);
-            session.setAttribute("pass",password);
+            HttpSession session = request.getSession();
+            session.setAttribute("uname", username);
+            session.setAttribute("pass", password);
 
 
-
-        }else{
+        } else {
             out.println("<br>Sorry! invalid details");
             // out.println("Sorry invalid username and password");
-            RequestDispatcher requestDispatcher=request.getRequestDispatcher("/login.html");
-            requestDispatcher.include(request,response);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.html");
+            requestDispatcher.include(request, response);
         }
         out.close();
 
-}
-
-
+    }
 }
