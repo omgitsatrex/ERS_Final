664bd0b429d0292ca65eb6778e156f4e1d0bc64d
Elliot Culbertson <elliot@gmail.com>
2022-03-07 03:02:33 -0500

    manager login via sql (WIP)

---
+++ project1/src/main/java/ManagerLogin.java
@@ -0,0 +1,46 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class ManagerLogin extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        request.getRequestDispatcher("navbar.html").include(request, response);
+        out.println("<br>test1");
+
+//        int id = Integer.parseInt(request.getParameter("id"));
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+//        Manager manager = new Manager();//(id, username, password);
+//        manager.setUsername(username);
+//        manager.setPassword(password);
+
+        Configuration config = new Configuration();
+
+        config.configure("hibernate.cfg.xml");
+
+        SessionFactory factory = config.buildSessionFactory();
+
+        Session session = factory.openSession();
+
+        List <Manager> list = session.createQuery("from Manager", Manager.class).list();
+
+//        out.println(list.toString()); // getting [Employee@20243220]
+
+        for(Manager e:list){
+            out.println( "<br>" + e.toString());
+        }
+        out.println("<br>test2");
+    }
+}
